7a6e5abece9445b3d3f36116e6905009d0159c7b
honghaine <honghai.sg99@gmail.com>
2024-10-10 10:00:29 +0700

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-2, 0, 3, -5, 2, -1};
+        NumArray obj = new NumArray(nums);
+    }
+}
---
+++ src/NumArray.java
@@ -0,0 +1,19 @@
+public class NumArray {
+    int[] nums;
+
+    public NumArray(int[] nums) {
+        this.nums = nums;
+    }
+
+    public int sumRange(int left, int right) {
+        int[] P = new int[nums.length];
+        P[0] = this.nums[left];
+        int sum = 0;
+        for (int i = left; i <right+1 ; i++) {
+
+
+        }
+
+        return 1;
+    }
+}
