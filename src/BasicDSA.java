7d41f68c8aba7ccc4e2c58e048cb5f79b9489516
honghaine <honghai.sg99@gmail.com>
2024-10-10 10:56:28 +0700

    Commit two sum

--- src/BasicDSA.java
+++ src/BasicDSA.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class BasicDSA {
+import java.util.HashMap;
+
+public class BasicDSA {
+
+    /**
+     * https://leetcode.com/problems/two-sum/
+     * https://algo.monster/liteproblems/1
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        if (nums.length < 2) {
+            return result;
+        }
+
+        HashMap<Integer, Integer> position = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+
+            int y = target - nums[i];
+
+            if (position.containsKey(y)) {
+                result[0] = position.get(y);
+                result[1] = i;
+                return result;
+            }
+
+            position.put(nums[i], i);
+        }
+        return result;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {-2, 0, 3, -5, 2, -1};
-        NumArray obj = new NumArray(nums);
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        BasicDSA basicDSA = new BasicDSA();
+        int[] a = basicDSA.twoSum(nums, target);
+        for (int b:a) {
+            System.out.println(b);
+        }
     }
 }
